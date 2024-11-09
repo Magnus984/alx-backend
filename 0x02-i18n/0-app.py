8431e4e2e238343f0c17b7d706dc8080c1bacac7
Magnus984 <nharnharyhaw984@gmail>
2024-11-09 10:01:10 +0000

    Added annotation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def hello_world() -> Response:
     """Renders index.html"""
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
