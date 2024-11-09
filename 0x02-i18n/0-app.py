3cd073aa8622f6330b15eb11f618e3e23da8ba93
Magnus984 <nharnharyhaw984@gmail>
2024-11-09 10:30:33 +0000

    Fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, render_template, Response
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world() -> Response:
+@app.route("/", methods=["GET"])
+def hello_world():
     """Renders index.html"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=5001)
