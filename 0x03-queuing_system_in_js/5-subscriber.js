02330704d75e56206cd03b468cb384fc5e16365b
Magnus984 <nharnharyhaw984@gmail>
2024-12-03 19:36:33 +0000

    add

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,21 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (_err, msg) => {
+  console.log(msg);
+  if (msg === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
